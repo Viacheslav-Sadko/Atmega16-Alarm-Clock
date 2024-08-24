3b7628db6a0acf9938eeef993f6968fcd79d2916
Viacheslav Sadko <73247577+Viacheslav-Sadko@users.noreply.github.com>
2024-08-24 21:11:44 +0300

    Add menu body

--- Debug/main.d
+++ Debug/main.d
@@ -14,6 +14,8 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  ../Lib/Inc/main.h ../Lib/Inc/Buzzer.h ../Lib/Inc/LCD.h ../Lib/Inc/main.h \
  ../Lib/Inc/UART.h ../Lib/Inc/MCU_Init.h ../Lib/Inc/UART.h \
  ../Lib/Inc/I2C.h ../Lib/Inc/I2C.h ../Lib/Inc/AT24C32.h \
@@ -49,6 +51,10 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
 ../Lib/Inc/main.h:
 
